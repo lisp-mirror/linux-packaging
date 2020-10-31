426bbb61a378d3464d312ce00cb499381dc28644
Florian Margaine <florian@platform.sh>
2020-10-31 15:52:53 +0100

    Add a way to provide arbitrary dependencies.

--- .ci/tests/pacman.sh
+++ .ci/tests/pacman.sh
@@ -7,3 +7,6 @@ test -f *.pkg.tar.xz
 pacman --noconfirm -U *.pkg.tar.xz
 
 test $(pacman -Qi random-name | grep -c sqlite) = 1
+
+# additional-dependencies
+test $(pacman -Qi random-name | grep -c emacs) = 1
