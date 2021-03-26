9f4b0c303ef2f265248b864bf10ec6aae1e6f0d4
Florian Margaine <florian@margaine.com>
2021-03-26 16:09:45 +0000

    Fix resolution of nested symlinks.
    And simplify the code with this magic function that does everything we need.

--- deb.lisp
+++ deb.lisp
@@ -1,8 +1,7 @@
 (uiop:define-package :linux-packaging/deb
     (:use :cl :linux-packaging/package)
-  (:import-from :uiop #:run-program)
+  (:import-from :uiop #:run-program #:truenamize)
   (:import-from :cl-ppcre #:split)
-  (:import-from :sb-posix #:s-islnk #:stat-mode #:lstat #:readlink)
   (:export #:deb))
 
 (in-package :linux-packaging/deb)
@@ -13,12 +12,9 @@
 (defmethod system-dependencies ((s deb))
   #+sbcl '("libc6" "zlib1g"))
 
-(defun symlinkp (pathname)
-  (s-islnk (stat-mode (lstat pathname))))
-
 (defmethod path->package ((s deb) path)
   ;; output is: "<package>:amd64: <file>"
   (first (split
 	  ":"
-	  (run-program `("dpkg" "-S" ,(if (symlinkp path) (namestring (merge-pathnames (readlink path) path)) path))
+	  (run-program `("dpkg" "-S" ,(namestring (truenamize path))
 		       :output '(:string :stripped t)))))
